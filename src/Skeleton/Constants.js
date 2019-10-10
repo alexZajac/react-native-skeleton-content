00899de754991f28fa1ac88940eb235006d88476
Alexandre Zajac <zajac.alexandre@gmail.com>
2019-10-10 01:52:51 -0700

    Switched to TypeScript

--- src/Skeleton/Constants.js
+++
@@ -1,9 +0,0 @@
-import { Easing } from 'react-native';
-
-export const DEFAULT_BORDER_RADIUS = 4;
-export const DEFAULT_DURATION = 1200;
-export const DEFAULT_ANIMATION_TYPE = 'shiver';
-export const DEFAULT_ANIMATION_DIRECTION = 'horizontalRight';
-export const DEFAULT_BONE_COLOR = '#E1E9EE';
-export const DEFAULT_HIGHLIGHT_COLOR = '#F2F8FC';
-export const DEFAULT_EASING = Easing.bezier(0.5, 0, 0.25, 1);
