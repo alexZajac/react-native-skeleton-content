858ac5c5edb688c7e7483696428b5758cc081ff7
Alexandre Zajac <work@alexandrezajac.com>
2020-06-30 00:02:37 +0200

    :white_check_mark: fixed constants types

--- src/Constants.ts
+++ src/Constants.ts
@@ -38,8 +38,8 @@ export interface IDirection {
 
 export const DEFAULT_BORDER_RADIUS = 4;
 export const DEFAULT_DURATION = 1200;
-export const DEFAULT_ANIMATION_TYPE: animationType = 'shiver';
-export const DEFAULT_ANIMATION_DIRECTION: animationDirection =
+export const DEFAULT_ANIMATION_TYPE: _animationType = 'shiver';
+export const DEFAULT_ANIMATION_DIRECTION: _animationDirection =
   'horizontalRight';
 export const DEFAULT_BONE_COLOR = '#E1E9EE';
 export const DEFAULT_HIGHLIGHT_COLOR = '#F2F8FC';
